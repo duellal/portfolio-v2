53d3f6ac99dc2f298fea72f72c2d460b3be8f832
Alex <alexandriaduell@gmail.com>
2026-06-26 20:10:44 -0400

    Fixed rebounding header on scroll and changed color or background and border based on where the user is on the page

--- src/app/header.js
+++ src/app/header.js
@@ -4,8 +4,9 @@ import Link from "next/link";
 import { useEffect, useRef, useState } from "react";
 
 export default function Header() {
-    const [isVisible, setIsVisible] = useState(true);
     const [headerIsVisible, setHeaderIsVisible] = useState(true);
+    const [isHidden, setIsHidden] = useState(false)
+    const [isVisible, setIsVisible] = useState(true);
     const lastClientYRef = useRef(0);
 
     useEffect(() => {
@@ -14,14 +15,15 @@ export default function Header() {
             const clientY = evt.clientY ?? lastClientYRef.current;
             lastClientYRef.current = clientY;
 
-            const newHeaderIsVisible = scrollY < 500;
+            const newHeaderIsVisible = scrollY < 200;
 
             const shouldHideNav =
-                (scrollY >= 200 && clientY >= 100) ||
+                (scrollY >= 200 && clientY >= 150) ||
                 (scrollY >= 200 && evt.clientY === undefined);
 
             setHeaderIsVisible(newHeaderIsVisible);
             setIsVisible(!shouldHideNav);
+            setIsHidden(!newHeaderIsVisible && shouldHideNav)
         };
 
         window.addEventListener('scroll', controlNavbar);
@@ -39,16 +41,16 @@ export default function Header() {
 
             <header className={`flex w-full items-center justify-center 
                 ${isVisible ? 'translate-y-0' : '-translate-y-full ease-out duration-900'} 
-                ${headerIsVisible ? 'h-60' : 'top-10 sticky h-35'} 
+                ${headerIsVisible ? 'h-60' : isHidden ? 'invisible' : 'top-0 sticky h-35'} 
                 group`}
             >
                 < nav
-                    className={`h-22 w-[80%] motion-reduce:duration-0 items-center justify-between shadow-white/10 shadow-lg bg-white/50 backdrop-blur-lg rounded-full 
-                    ${headerIsVisible ? 'transition-none' : isVisible ? 'top-0 translate-y-0' : '-translate-y-full'} 
+                    className={`h-22 w-[80%] motion-reduce:duration-0 items-center justify-between shadow-lg bg-white/5 backdrop-blur-lg rounded-full 
+                    ${headerIsVisible ? 'transition-none shadow-(--mesh-navy)/10' : isVisible ? 'top-0 translate-y-0 shadow-white/10' : '-translate-y-full'} 
                     transition-transform transition-discrete ease-in-out duration-900`}
                 >
-                    <Link className="text-teal-600 dark:text-teal-600" href={""}>
-                        <span className="sr-only">Home</span>
+                    <Link className="" href={""}>
+                        <span className="">Home</span>
                     </Link>
                 </nav>
             </header >
